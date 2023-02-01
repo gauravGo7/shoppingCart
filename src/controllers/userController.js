db84bc157d8609207964d9f7ed053ad36f35f4bb
gauravGo7 <grvgoswami2007@gmail.com>
2023-02-02 00:23:11 +0530

    login done

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -103,4 +103,50 @@ exports.createUser = async function (req, res) {
       return res.status(500).send({ status: false, message: error.message });
     }
 }
+
+
+
+//------------------------------------------------------------Login Api-------------------------------------------------------------------------------
+
+exports.loginUser = async function (req, res) {
+  try {
+      let { email, password } = req.body;
+
+      if (Object.keys(req.body).length === 0) {
+          return res.status(400).send({ status: false, message: "please input user Details" });
+      }
+
+      if (!email) {
+          return res.status(400).send({ status: false, message: "EmailId is mandatory", });
+      }
+      if (!validEmail(email)) {
+          return res.status(400).send({ status: false, message: "EmailId should be Valid", });
+      }
+      if (!password) {
+          return res.status(400).send({ status: false, message: "Password is mandatory" });
+      }
+      if (password.length < 8 || password.length > 15) {
+          return res.status(400).send({ status: false, message: "the length of password must be min: 8 or max: 15", });
+      }
+
+      let verifyUser = await userModel.findOne({ email: email });
+      if (!verifyUser) return res.status(400).send({ status: false, message: "Invalid Login Credential" });
+
+      //-------------------------------------------Decrypt the password and compare the password with user input------------------------------------------//
+      bcrypt.compare(password, verifyUser.password, function (error, verifyUser) {
+          if (error) return res.status(400).send({ status: false, message: error.message })
+      });
+
+
+      let payload = { userId: verifyUser["_id"], iat: Date.now(), };
+
+
+      let token = jwt.sign(payload, "Group15", { expiresIn: "24h" });
+
+      res.setHeader("authorization", token);
+      res.status(200).send({ status: true, message: "User login successfull", data: { userId: verifyUser["_id"], token } });
+  } catch (error) {
+      res.status(500).send({ status: false, message: error.message, });
+  }
+};
   
