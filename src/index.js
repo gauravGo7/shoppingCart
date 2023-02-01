021fc5b0a08be8a910674798bc6ecfa9b346b8f8
gauravGo7 <grvgoswami2007@gmail.com>
2023-02-02 00:49:05 +0530

    project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-const express=require('express')
-const mongoose=require('mongoose')
-const router=require('./router/route')
-const app=express()
+const express = require('express')
+const mongoose = require('mongoose')
+const router = require('./router/route')
+const app = express()
 app.use(express.json())
 mongoose.set('strictQuery', true);
 mongoose.connect("mongodb+srv://gaurav:Grv20072000@cluster0.3fqqw8s.mongodb.net/group15Database")
-.then(()=>console.log("mongoDB is connected"))
-.catch((err)=>console.error(err))
+    .then(() => console.log("mongoDB is connected"))
+    .catch((err) => console.error(err))
 
-app.use("/",router)
-app.listen(3000,()=>{
-    console.log("express app running on port"+3000)
+app.use("/", router)
+app.listen(3000, () => {
+    console.log("express app running on port" + 3000)
 })
