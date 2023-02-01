fc30163f7b713ee2acd63aeb7a354a4de2fe6300
AshanurHossain0 <ashanurhossain0@gmail.com>
2023-02-01 18:21:26 +0530

    authenticationDone

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,1 +1,18 @@
-const a=1
+const userModel = require("../models/userModel");
+
+exports.getProfile = async function (req, res) {
+    try {
+        const userId = req.params.userId;
+
+        if (!ObjectId.isValid(userId)) return res.status(400).send({ status: false, message: "Please provide valid user id" })
+
+        const loggedUser = req.token.userId;
+        if (userId != loggedUser) return res.status(403).send({ status: false, message: "you can't see other people's profile" })
+
+        const profileData = await userModel.findById(userId)
+        return res.status(200).send({ status: true, message: "User profile details", data: profileData })
+    }
+    catch(err){
+        return res.status(500).send({status:false,message:err.message})
+    }
+}
--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,1 +1,30 @@
 const jwt = require('jsonwebtoken');
+
+exports.authentication = async function (req, res) {
+    try {
+        let token = req.headers.authorization
+        if (!token) return res.status(400).send({ status: false, message: "token is not given" })
+        token = token.slice(7);
+
+
+        jwt.verify(token, "Group15", function (err, decodedToken) {
+            if (err) {
+                if (err.name === 'JsonWebTokenError') {
+                    return res.status(401).send({ status: false, message: "invalid token" });
+                }
+
+                if (err.name === 'TokenExpiredError') {
+                    return res.status(401).send({ status: false, message: "you are logged out, login again" });
+                } else {
+                    return res.send({ msg: err.message });
+                }
+            } else {
+                req.token = decodedToken;
+                next();
+            }
+        });
+    }
+    catch(err){
+        return res.status(500).send({status:false,message:err.message})
+    }
+}
