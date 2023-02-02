7c9c68fa5a4831042987d5355b4b743389aaed37
Tirthankar Roy <roytirthankar2@gmail.com>
2023-02-02 16:23:11 +0530

    delete api

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,4 +1,21 @@
 const productModel = require("../models/productModel")
+const {validObjectId } = require("../validator/validation")
+
+exports.deleteProduct = async function (req, res) {
+    try {
+      let productId = req.params.productId
+      if (!validObjectId(productId)) return res.status(400).send({ status: false, message: 'ProductId is not Valid' })
+      
+      let productData = await productModel.findOne({_id:productId,isDeleted:false})
+      if (!productData) return res.status(404).send({ status: false, message: "Product Not Found" })
+      
+      await productModel.findOneAndUpdate({ _id: productId }, { $set: { isDeleted: true, deletedAt: new Date() } }, { new: true })
+      return res.status(200).send({ status: true, message: "Product Deleted Successfully" })
+    }
+    catch (err) {
+      return res.status(500).send({ status: false, message: err.message });
+    }
+  }
 
 
 
