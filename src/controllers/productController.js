102163a642bc1c530f3afb26f16781b4de7d2cee
gauravGo7 <grvgoswami2007@gmail.com>
2023-02-02 18:42:54 +0530

    project

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,31 @@
 const productModel = require("../models/productModel")
-const { validObjectId } = require("../validator/validation")
+const { validObjectId  } = require("../validator/validation")
+exports.createProduct = async (req, res) => {
+try {
+
+    let data = req.body
+
+    let { title, description, price, currencyId, currencyFormat, isFreeShipping, style, availableSizes, installments, productImage } = data
+
+    if (Object.keys(data).length == 0) return res.status(400).send({ status: false, message: "No data found from body! You need to put the Mandatory Fields. " });
+
+    let obj = {}
+
+    const isDuplicateTitle = await productModel.findOne({ title: title });
+    if (isDuplicateTitle) {
+        return res.status(400).send({ status: false, message: "Title is Already Exists, Please Enter Another Title!" });
+    }
+
+    let createProduct = await productModel.create(obj)
+
+    return res.status(201).send({ status: true, message: "Success", data: createProduct })
+
+} catch (error) {
+
+    return res.status(500).send({ status: false, error: error.message })
+}
+}
+
 
 exports.getProductById = async function (req, res) {
     try {
