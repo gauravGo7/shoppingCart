b9509a9e200423bc8b8a13bb4ae760600b1a0224
Tirthankar Roy <roytirthankar2@gmail.com>
2023-02-02 12:14:01 +0530

    user done

--- src/router/route.js
+++ src/router/route.js
@@ -1,5 +1,8 @@
 const express=require('express')
 const router=express.Router()
+const {createUser} = require("../controllers/userController")
+
+router.post("/register", createUser)
 
 router.all("/*",(req,res)=>{
     res.status(404).send({msg:"invalid http request"})
