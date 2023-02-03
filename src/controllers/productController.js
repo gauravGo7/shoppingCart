1d473a97d41ce8d83e25b8409345325c37866023
AshanurHossain0 <ashanurhossain0@gmail.com>
2023-02-03 11:42:51 +0530

    done

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -76,10 +76,9 @@ exports.deleteProduct = async function (req, res) {
         let productId = req.params.productId
         if (!validObjectId(productId)) return res.status(400).send({ status: false, message: 'ProductId is not Valid' })
 
-        let productData = await productModel.findOne({ _id: productId, isDeleted: false })
+        let productData = await productModel.findOneAndUpdate({ _id: productId }, { $set: { isDeleted: true, deletedAt: new Date() } })
         if (!productData) return res.status(404).send({ status: false, message: "Product Not Found" })
 
-        await productModel.findOneAndUpdate({ _id: productId }, { $set: { isDeleted: true, deletedAt: new Date() } }, { new: true })
         return res.status(200).send({ status: true, message: "Product Deleted Successfully" })
     }
     catch (err) {
